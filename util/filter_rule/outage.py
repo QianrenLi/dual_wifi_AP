7d42b28004e5bb15d969e373d5b004ac1fba5e5d
Qianrenli <908559578@qq.com>
2025-11-05 17:16:08 +0800

    chore: constrainded outage

--- util/filter_rule/outage.py
+++ util/filter_rule/outage.py
@@ -7,16 +7,22 @@ def _is_num(x: Any) -> bool:
     return isinstance(x, (int, float))
 
 @register_filter
-def scale_outage(value: Any, zeta: float = 1.0):
+def scale_outage(value: Any, zeta: float = 1.0, minimum = None):
     """
     Compute zeta * outage_rate. Works for scalar or dict-of-scalars.
     """
+    def clip_minimum(val, minimum):
+        if minimum == None:
+            return val
+        else:
+            return max(val, minimum)
+        
     if _is_num(value):
-        return zeta * float(value)
+        return clip_minimum(zeta * float(value))
     if isinstance(value, dict):
         out = {}
         for k, v in value.items():
             if _is_num(v):
-                out[str(k)] = zeta * float(v)
+                out[str(k)] = clip_minimum(zeta * float(v))
         return out
     return None
