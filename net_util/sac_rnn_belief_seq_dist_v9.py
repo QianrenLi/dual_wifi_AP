82b0457a6e65f89087be6cc9f24a28b99239c867
Qianrenli <908559578@qq.com>
2025-12-03 20:44:47 +0800

    chore

--- net_util/sac_rnn_belief_seq_dist_v9.py
+++ net_util/sac_rnn_belief_seq_dist_v9.py
@@ -366,7 +366,7 @@ class SACRNNBeliefSeqDistV9(PolicyBase):
             return
 
         # 1) Get z from belief encoder (no grad, just a probe)
-        z_BH, _, _, _ = self.net.belief_encode(obs_TBD, b_h=h_burn)      # [T,B,H_z] or [B,H_z] depending on impl
+        z_BH, _, _, _ = self.net.belief_encode(obs_TBD, b_h=h_burn, is_evaluate=True)      # [T,B,H_z] or [B,H_z] depending on impl
 
         # 2) Features + actions with real z
         feat_full_TBH, _ = self.net.feature_compute(obs_TBD, z_BH, f_h_burn)  # [T,B,H]
